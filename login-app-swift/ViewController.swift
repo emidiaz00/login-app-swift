2023bc325e1617e479208af86806539a2fc51437
emildsn <emildsn00@gmail.com>
2023-03-10 20:49:12 -0300

    add constraints responsives

--- login-app-swift/ViewController.swift
+++ login-app-swift/ViewController.swift
@@ -22,6 +22,6 @@ class ViewController: UIViewController {
         let loginVC = storyboard.instantiateViewController(
             withIdentifier: "LoginViewController")
         self.present(loginVC, animated: true, completion: nil)
-    }    
+    }
 }
 
